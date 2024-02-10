d452420f1abb2ea26fa621831a5886ecb45b50fd
pankajprasad30 <pankajprasad30@gmail.com>
2024-02-10 17:15:23 +0530

    Java Code Pusing to GitHub

---
+++ CTE_Constructor.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class CTE_Constructor 
+{
+	CTE_Constructor (int a)
+	{
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) 
+	{
+		CTE_Constructor p=new CTE_Constructor ();
+		p.main(null);
+		
+
+	}
+
+}
